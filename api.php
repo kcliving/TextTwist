7e569da45f8b2e92b21bc7a5ddfbdf4c36aafbd8
kcliving <42876416+kcliving@users.noreply.github.com>
2019-10-03 14:49:44 -0400

    Update api.php

--- api.php
+++ api.php
@@ -4,9 +4,13 @@
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
     $query = "SELECT rack FROM racks WHERE length=7 order by random() limit 0, 1";
+    $query1 = "SELECT words From racks Where rack =$query;
     $statement = $dbhandle->prepare($query);
+    $statement2 = $dbhandle->prepare($query2);
     $statement->execute();
+    $statment2->execute;
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($results);
+    $results2 = $statement->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($results, $results2);
     
 ?>
