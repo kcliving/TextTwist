b99eab3124339894fdd0779a8602049a741b2e43
kcliving <42876416+kcliving@users.noreply.github.com>
2019-10-03 14:51:23 -0400

    Update api.php

--- api.php
+++ api.php
@@ -3,14 +3,10 @@
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
-    $query = "SELECT rack FROM racks WHERE length=7 order by random() limit 0, 1";
-    $query1 = "SELECT words From racks Where rack =$query;
+    $query = "SELECT rack, words FROM racks WHERE length=7 order by random() limit 0, 1";
     $statement = $dbhandle->prepare($query);
-    $statement2 = $dbhandle->prepare($query2);
     $statement->execute();
-    $statment2->execute;
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $results2 = $statement->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($results, $results2);
+    echo json_encode($results);
     
 ?>
